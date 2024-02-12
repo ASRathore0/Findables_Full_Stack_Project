121f0a985f08b370df0a0c9488b310b926927ac0
S A U R A V <sauravchp2@gmail.com>
2024-02-13 02:06:00 +0530

    feat(Pagination): Add Pagination component and styles
    - Implement Pagination component to provide navigation controls for page
      navigation
    - Style Pagination component to display buttons for previous and next
      pages
    - Utilize Next.js useRouter hook for client-side navigation

--- src/componenets/pagination/Pagination.jsx
+++ src/componenets/pagination/Pagination.jsx
@@ -1,8 +1,34 @@
-import react from "react";
-import styles from "./pagination.module.css";
+"use client";
 
-const Pagination = () => {
-    return <div className={styles.container}>Pagination</div>;
+import React from "react";
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
+
+// Pagination component representing pagination controls
+const Pagination = ({ page, hasPrev, hasNext }) => {
+    // Initializing useRouter hook to access Next.js router object
+    const router = useRouter();
+
+    return (
+        <div className={styles.container}>
+            {/* Button for navigating to the previous page */}
+            <button
+                className={styles.button}
+                disabled={!hasPrev}
+                onClick={() => router.push(`?page=${page - 1}`)}
+            >
+                Previous
+            </button>
+            {/* Button for navigating to the next page */}
+            <button
+                disabled={!hasNext}
+                className={styles.button}
+                onClick={() => router.push(`?page=${page + 1}`)}
+            >
+                Next
+            </button>
+        </div>
+    );
 };
 
 export default Pagination;
