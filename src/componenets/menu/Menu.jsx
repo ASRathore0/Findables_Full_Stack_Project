32ec2c63a6d934de18d1e4bd50727a17eac3bfb7
S A U R A V <sauravchp2@gmail.com>
2024-02-13 02:03:13 +0530

    feat(Menu): Add Menu component and related components
    - Add Menu component to render the menu section of the application
    - Include MenuPosts and MenuCategories component to display lists of
      posts and categories respectively
    - Utilize Next.js routing for navigation between pages

--- src/componenets/menu/Menu.jsx
+++ src/componenets/menu/Menu.jsx
@@ -1,8 +1,25 @@
-import react from "react";
+import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
+// Menu component representing the menu section of the application
 const Menu = () => {
-    return <div className={styles.container}>Menu</div>;
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.subtitle}>{"What's hot"}</h2>
+            <h1 className={styles.title}>Most Popular</h1>
+            <MenuPosts withImage={false} />
+            <h2 className={styles.subtitle}>Discover by topic</h2>
+            <h1 className={styles.title}>Categories</h1>
+            <MenuCategories />
+            <h2 className={styles.subtitle}>Chosen by the editor</h2>
+            <h1 className={styles.title}>Editors Pick</h1>
+            <MenuPosts withImage={true} />
+        </div>
+    );
 };
 
 export default Menu;
