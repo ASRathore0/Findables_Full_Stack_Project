76f55548b9fd08c53b3d4d1d4be3d584389320b2
S A U R A V <sauravchp2@gmail.com>
2024-02-18 11:42:14 +0530

    feat: Add COmments component and update styles for [slug]/page.jsx
    Changes:
    - Added import statement for Comments component in [slug]/page.jsx
    - Updated styles and layout in [slug]/singlePage.module.css for better readability and responsiveness.

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,34 +1,77 @@
-import Menu from '@/componenets/menu/Menu';
-import styles from "./singlePage.module.css"
-import Image  from "next/image";
+import Menu from "@/componenets/menu/Menu";
+import styles from "./singlePage.module.css";
+import Image from "next/image";
+import Comments from "@/componenets/comments/Comments";
 
 const SinglePage = () => {
     return (
         <div className={styles.contanier}>
             <div className={styles.infoContainer}>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.title}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
+                    <h1 className={styles.title}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit.
+                    </h1>
                     <div className={styles.user}>
                         <div className={styles.userImageContainer}>
-                            <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
-                         </div>
-                        <div className={styles.userTextContainer}>
-                           <span className={styles.userName}>John Doe</span>
-                           <span className={styles.date}>18.02.2024</span>
+                            <Image
+                                src="/p1.jpeg"
+                                alt=""
+                                fill
+                                className={styles.avatar}
+                            />
                         </div>
-                     </div>
+                        <div className={styles.userTextContainer}>
+                            <span className={styles.userName}>John Doe</span>
+                            <span className={styles.date}>18.02.2024</span>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.imageContainer}>
-                    <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-                    </div>
+                    <Image
+                        src="/p1.jpeg"
+                        alt=""
+                        fill
+                        className={styles.image}
+                    />
+                </div>
             </div>
             <div className={styles.content}>
-                <div className={styles.post}></div>
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Magni aut nemo corrupti impedit perferendis
+                            eum. Adipisci totam ea excepturi voluptates, eveniet
+                            iusto maxime ullam atque architecto ducimus numquam
+                            tempora voluptas.
+                        </p>
+                        <h2>Lorem ipsum dolor sit amet.</h2>
+
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Magni aut nemo corrupti impedit perferendis
+                            eum. Adipisci totam ea excepturi voluptates, eveniet
+                            iusto maxime ullam atque architecto ducimus numquam
+                            tempora voluptas.
+                        </p>
+
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Magni aut nemo corrupti impedit perferendis
+                            eum. Adipisci totam ea excepturi voluptates, eveniet
+                            iusto maxime ullam atque architecto ducimus numquam
+                            tempora voluptas.
+                        </p>
+                    </div>
+                    <div>
+                        <Comments />
+                    </div>
+                </div>
                 <Menu />
             </div>
-        </div>        
+        </div>
+    );
+};
 
-  )
-    }
-    
-    export default SinglePage
+export default SinglePage;
